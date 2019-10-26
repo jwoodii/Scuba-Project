e0cfe56730f98b0f002b0ca66c45c704ab222029
Jeffery Wood <35505841+jwoodii@users.noreply.github.com>
2019-10-25 20:17:35 -1000

    ...
    Failed attempt

---
+++ src/Test.java
@@ -0,0 +1,14 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.Scanner;
+public class Test {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		// TODO Auto-generated method stub
+		File excel = new File("./ScubaTable.xlsx");
+		Scanner file = new Scanner(excel);
+
+	}
+
+}
